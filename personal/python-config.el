101a81b14dc284f66e06203be65f8b9458dd2d41
Handol Park <handol@ston-ai.com>
2025-05-25 18:24:34 +0000

    feat(python): Add Ruff linting and formatting
    Enable Ruff for code linting via Flymake and formatting.  LSP
    configurations for `lsp-mode` and `lsp-bridge` were explored but
    remain disabled for now.

--- personal/python-config.el
+++ personal/python-config.el
@@ -6,9 +6,51 @@
   (treesit-install-language-grammar 'python))
 
 (use-package eglot
-  :hook ((python-mode . eglot-ensure)
-         (python-ts-mode . eglot-ensure))
+  :hook (((python-mode python-ts-mode) . eglot-ensure))
   :config
   (add-to-list 'eglot-server-programs
                '((python-mode python-ts-mode)
                  "basedpyright-langserver" "--stdio")))
+
+;; (use-package lsp-mode
+;;   :ensure t
+;;   :hook (((python-mode python-ts-mode) . lsp)
+;;          (lsp-mode . lsp-enable-which-key-integration))
+;;   :commands lsp)
+
+;; (use-package lsp-ui
+;;   :ensure t
+;;   :commands lsp-ui-mode)
+
+;; (use-package lsp-pyright
+;;   :ensure t
+;;   :custom ((lsp-pyright-langserver-command "basedpyright")
+;;            (lsp-pyright-basedpyright-inlay-hints-generic-types t))
+;;   :hook ((python-mode python-ts-mode) . (lambda ()
+;;                                           (require 'lsp-pyright)
+;;                                           (lsp))))
+
+
+;; NOTE: `lsp-bridge' is advised not to byte-compile the package but not
+;; found to avoid byte-compilation with vc-use-package (2025-05-23 PDT)
+
+;; (unless (package-installed-p 'vc-use-package)
+;;   (package-vc-install "https://github.com/slotThe/vc-use-package"))
+;; (require 'vc-use-package)
+
+;; (use-package yasnippet
+;;   :ensure t)
+
+;; (use-package lsp-bridge
+;;   :vc (lsp-bridge :url "https://github.com/manateelazycat/lsp-bridge.git")
+;;   :after (yasnippet markdown-mode)
+;;   :config
+;;   (global-lsp-bridge-mode))
+
+
+(use-package flymake-ruff
+  :ensure t
+  :hook (eglot-managed-mode . flymake-ruff-load))
+
+(use-package ruff-format
+  :ensure t)
